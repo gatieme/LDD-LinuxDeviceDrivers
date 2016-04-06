0991de0adb1b9d3562a323240fbfab0483fa144c
gatieme <gatieme@163.com>
2016-04-06 16:07:23 +0800

    内核锁与进程间通信...

---
+++ study/driver/probe/kprobe/.tmp_versions/kprobe-exam.mod
@@ -0,0 +1,2 @@
+/home/gatieme/Work/GitHub/LDD-LinuxDeviceDrivers/study/driver/probe/kprobe/kprobe-exam.ko
+/home/gatieme/Work/GitHub/LDD-LinuxDeviceDrivers/study/driver/probe/kprobe/kprobe-exam.o
---
+++ study/driver/probe/kprobe/kprobe-exam.mod.c
@@ -0,0 +1,32 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x146be775, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0xfd766812, __VMLINUX_SYMBOL_STR(unregister_kprobe) },
+	{ 0x4f68e5c9, __VMLINUX_SYMBOL_STR(do_gettimeofday) },
+	{ 0x1fb91136, __VMLINUX_SYMBOL_STR(register_kprobe) },
+	{ 0xe007de41, __VMLINUX_SYMBOL_STR(kallsyms_lookup_name) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
