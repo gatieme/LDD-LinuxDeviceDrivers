dbabd18a46e04bd81c073d0ea7981ea370acfb25
gatieme <gatieme@163.com>
2017-03-23 20:53:28 +0800

    hello world中添加了作者信息, makefile中添加编译参数...

--- study/driver/hello/hello.c
+++ study/driver/hello/hello.c
@@ -1,7 +1,15 @@
 #include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/sched.h>
+//#include <linux/list.h>
+//#include <linux/mm.h>
+//#include <linux/mm_types.h>
+
 
 MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("Gatieme");
+MODULE_DESCRIPTION("hello world");
 
 /*
  * print the module information
@@ -36,6 +44,7 @@ static int hello_init(void)
 
 static void hello_exit(void)
 {
+    printk(KERN_ERR"exit");
 }
 
 
