9c33abac953e24f0cd61ca9ca40e59168ae65d06
gatieme <gatieme@163.com>
2017-03-25 18:16:24 +0800

    debugfs...

---
+++ study/debug/debugfs/README.md
@@ -0,0 +1,37 @@
+You-Get--基于Python3的开源网络视频下载工具
+=======
+
+| CSDN | GitHub |
+|:----:|:------:|
+| [You-Get--基于Python3的开源网络视频下载工具](http://blog.csdn.net/gatieme/article/details/61623891) | [`AderXCoding/system/tools/you-get`](https://github.com/gatieme/AderXCoding/tree/master/system/tools/you-get) |
+
+<br>
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>
+本作品采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议</a>进行许可, 转载请注明出处, 谢谢合作
+<br>
+
+
+[Linux内核里的DebugFS](http://www.cnblogs.com/wwang/archive/2011/01/17/1937609.html)
+
+[Debugging the Linux Kernel with debugfs](http://opensourceforu.com/2010/10/debugging-linux-kernel-with-debugfs/)
+
+[debugfs-seq_file](http://lxr.free-electrons.com/source/drivers/base/power/wakeup.c)
+
+
+[Linux Debugfs文件系统介绍及使用](http://blog.sina.com.cn/s/blog_40d2f1c80100p7u2.html)
+
+[Linux 文件系统：procfs, sysfs, debugfs 用法简介](http://www.tinylab.org/show-the-usage-of-procfs-sysfs-debugfs/)
+
+[用户空间与内核空间数据交换的方式(1)------debugfs](http://www.cnblogs.com/hoys/archive/2011/04/10/2011124.html)
+
+
+[Linux 运用debugfs调试方法](http://www.xuebuyuan.com/1023006.html)
+
+
+误删恢复
+----
+[linux 删除文件和目录与恢复详解](http://www.111cn.net/sys/linux/47629.htm)
+
+<br>
+
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>本作品采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议</a>进行许可, 转载请注明出处, 谢谢合作.
---
+++ study/debug/debugfs/debugfs/my_debugfs.c
@@ -0,0 +1,106 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/debugfs.h>
+#include <asm/uaccess.h>
+
+struct dentry *my_debugfs_root;
+
+u8                          a           = 0;
+char                        hello[32]   = "Hello world!\n";
+struct debugfs_blob_wrapper b;
+
+static int c_open(struct inode *inode, struct file *filp)
+{
+	filp->private_data = inode->i_private;
+	return 0;
+}
+
+static ssize_t c_read(struct file *filp, char __user *buffer,
+		size_t count, loff_t *ppos)
+{
+	if (*ppos >= 32)
+		return 0;
+	if (*ppos + count > 32)
+		count = 32 - *ppos;
+
+	if (copy_to_user(buffer, hello + *ppos, count))
+		return -EFAULT;
+
+	*ppos += count;
+
+	return count;
+}
+
+static ssize_t c_write(struct file *filp, const char __user *buffer,
+		size_t count, loff_t *ppos)
+{
+	if (*ppos >= 32)
+		return 0;
+	if (*ppos + count > 32)
+		count = 32 - *ppos;
+
+	if (copy_from_user(hello + *ppos, buffer, count))
+		return -EFAULT;
+
+	*ppos += count;
+
+	return count;
+}
+
+struct file_operations c_fops = {
+	.owner = THIS_MODULE,
+	.open = c_open,
+	.read = c_read,
+	.write = c_write,
+};
+
+static int __init mydebugfs_init(void)
+{
+	struct dentry *sub_dir, *r_a, *r_b, *s_c;
+
+        printk(KERN_INFO "mydebugfs_init\n");
+
+	my_debugfs_root = debugfs_create_dir("mydebug", NULL);
+	if (!my_debugfs_root)
+		return -ENOENT;
+
+	r_a = debugfs_create_u8("a", 0644, my_debugfs_root, &a);
+	if (!r_a)
+		goto Fail;
+
+	b.data = (void *)hello;
+	b.size = strlen(hello) + 1;
+	r_b = debugfs_create_blob("b", 0644, my_debugfs_root, &b);
+	if (!r_b)
+		goto Fail;
+
+	sub_dir = debugfs_create_dir("subdir", my_debugfs_root);
+	if (!sub_dir)
+		goto Fail;
+
+	s_c = debugfs_create_file("c", 0644, sub_dir, NULL, &c_fops);
+	if (!s_c)
+		goto Fail;
+
+        return 0;
+
+Fail:
+	debugfs_remove_recursive(my_debugfs_root);
+	my_debugfs_root = NULL;
+	return -ENOENT;
+}
+
+static void __exit mydebugfs_exit(void)
+{
+    printk(KERN_INFO "mydebugfs_exit\n");
+	debugfs_remove_recursive(my_debugfs_root);
+
+    return;
+}
+
+module_init(mydebugfs_init);
+module_exit(mydebugfs_exit);
+
+MODULE_LICENSE("GPL");
+
